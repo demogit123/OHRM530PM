1141d9b0df7e63188e0001c811b7296890138ff1
MAXHUB <MAXHUB@DESKTOP-0RQF8FK>
2023-08-22 19:08:29 +0530

    TestCase2 updated

--- OrangeHRM/src/testcases/Testcase2.java
+++ OrangeHRM/src/testcases/Testcase2.java
@@ -2,8 +2,9 @@ package testcases;
 
 public class Testcase2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("TestCase 2 Updated ");
 
 	}
 
